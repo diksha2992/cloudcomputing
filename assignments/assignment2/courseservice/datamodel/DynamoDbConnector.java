770812128520116a9a75ec52e40128ad604782c8
Sai Diksha <31628457+diksha2992@users.noreply.github.com>
2018-11-18 20:37:59 -0800

    Add files via upload

---
+++ assignments/assignment2/courseservice/datamodel/DynamoDbConnector.java
@@ -0,0 +1,27 @@
+package com.csye6225.fall2018.courseservice.datamodel;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+
+public class DynamoDbConnector {
+
+	static AmazonDynamoDB dynamoDB;
+
+	public static void init() {
+		if (dynamoDB == null) {
+			AWSCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+			credentialsProvider.getCredentials();
+
+			dynamoDB = AmazonDynamoDBClientBuilder.standard().withCredentials(credentialsProvider)
+					.withRegion("us-east-1").build();
+		}
+
+	}
+
+	public static AmazonDynamoDB getClient() {
+		init();
+		return dynamoDB;
+	}
+}
